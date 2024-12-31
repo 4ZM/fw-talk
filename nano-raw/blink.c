613df140e7c018276d1a1862aa0bce059bb566ca
Anders Sundman (asum) <anders.sundman@tobii.com>
2024-12-31 08:49:14 +0100

    uint typedef

--- nano-raw/blink.c
+++ nano-raw/blink.c
@@ -1,8 +1,8 @@
-typedef unsigned int *isr_vector;
+typedef unsigned int uint;
 
-extern unsigned int _data_start_flash, _data_start, _data_end;
-extern unsigned int _bss_start, _bss_end;
-extern unsigned int _stack_top;
+extern uint _data_start_flash, _data_start, _data_end;
+extern uint _bss_start, _bss_end;
+extern uint _stack_top;
 
 int main(void);
 
@@ -17,29 +17,29 @@ void DebugMon_Handler(void) __attribute__((alias("Default_Handler")));
 void PendSV_Handler(void) __attribute__((alias("Default_Handler")));
 void SysTick_Handler(void);
 
-__attribute__((section(".vectors"))) isr_vector isr_vectors[] = {
-    &_stack_top,
-    (isr_vector)Reset_Handler,
-    (isr_vector)NMI_Handler,
-    (isr_vector)HardFault_Handler,
-    (isr_vector)MemManage_Handler,
-    (isr_vector)BusFault_Handler,
-    (isr_vector)UsageFault_Handler,
-    0,
-    0,
-    0,
-    0, // Reserved
-    (isr_vector)SVC_Handler,
-    (isr_vector)DebugMon_Handler,
-    0, // Reserved
-    (isr_vector)PendSV_Handler,
-    (isr_vector)SysTick_Handler};
+__attribute__((section(".vectors")))
+uint *isr_vectors[] = {&_stack_top,
+                       (uint *)Reset_Handler,
+                       (uint *)NMI_Handler,
+                       (uint *)HardFault_Handler,
+                       (uint *)MemManage_Handler,
+                       (uint *)BusFault_Handler,
+                       (uint *)UsageFault_Handler,
+                       0,
+                       0,
+                       0,
+                       0, // Reserved
+                       (uint *)SVC_Handler,
+                       (uint *)DebugMon_Handler,
+                       0, // Reserved
+                       (uint *)PendSV_Handler,
+                       (uint *)SysTick_Handler};
 
 void Reset_Handler(void) {
 
   // Copy data section from Flash to RAM
-  unsigned int *src = &_data_start_flash;
-  unsigned int *dest = &_data_start;
+  uint *src = &_data_start_flash;
+  uint *dest = &_data_start;
   while (dest < &_data_end) {
     *dest++ = *src++;
   }
@@ -56,7 +56,7 @@ void Reset_Handler(void) {
   }
 }
 
-volatile unsigned int counter = 0;
+volatile uint counter = 0;
 
 void SysTick_Handler(void) {
   if (counter)
@@ -70,43 +70,43 @@ void Default_Handler(void) {
 
 int main(void) {
 
-  const unsigned int MS_TICK = 0xfa00; // 64MHz x 1ms => 64000 (0xfa00) ticks
+  const uint MS_TICK = 0xfa00; // 64MHz x 1ms => 64000 (0xfa00) ticks
 
   // Arm Cortex-M4 Technical Reference
   //   4.1 System control registers
-  const unsigned int STCSR = 0xE000E010; // Control and Status Register
-  const unsigned int STRVR = 0xE000E014; // Reload Value Register
-  const unsigned int STCVR = 0xE000E018; // Current Value Register
+  const uint STCSR = 0xE000E010; // Control and Status Register
+  const uint STRVR = 0xE000E014; // Reload Value Register
+  const uint STCVR = 0xE000E018; // Current Value Register
 
   // Arm7-M Architecture Reference Manual (B3.3.1 SysTick operation)
-  *((unsigned int *)STRVR) = MS_TICK - 1;
-  *((unsigned int *)STCVR) = 0;
+  *((uint *)STRVR) = MS_TICK - 1;
+  *((uint *)STCVR) = 0;
 
   // Arm7-M Architecture Reference Manual
   //   B3.3.3 SysTick Control and Status Register, SYST_CSR
-  unsigned int CLKSOURCE_BIT = 1 << 2; // 1 => SysTick uses the processor clock
-  unsigned int TICKINT_BIT = 1 << 1;   // 1 => SysTick exception to pending on 0
-  unsigned int ENABLE_BIT = 1 << 0;    // 1 => Counter is operating.
-  *((unsigned int *)STCSR) = CLKSOURCE_BIT | TICKINT_BIT | ENABLE_BIT;
+  uint CLKSOURCE_BIT = 1 << 2; // 1 => SysTick uses the processor clock
+  uint TICKINT_BIT = 1 << 1;   // 1 => SysTick exception to pending on 0
+  uint ENABLE_BIT = 1 << 0;    // 1 => Counter is operating.
+  *((uint *)STCSR) = CLKSOURCE_BIT | TICKINT_BIT | ENABLE_BIT;
 
   // IO
-  unsigned int gpio_p0 = 0x50000000;
-  unsigned int gpio_outset = 0x508;
-  unsigned int gpio_outclr = 0x50C;
-  unsigned int gpio_dirset = 0x518;
+  uint gpio_p0 = 0x50000000;
+  uint gpio_outset = 0x508;
+  uint gpio_outclr = 0x50C;
+  uint gpio_dirset = 0x518;
 
-  unsigned int led_builtin = 13; // P0.13 LED_BUILTIN (yellow)
+  uint led_builtin = 13; // P0.13 LED_BUILTIN (yellow)
 
-  *((unsigned int *)(gpio_p0 + gpio_dirset)) = 1 << led_builtin;
+  *((uint *)(gpio_p0 + gpio_dirset)) = 1 << led_builtin;
 
   for (;;) {
-    *((unsigned int *)(gpio_p0 + gpio_outset)) = 1 << led_builtin;
+    *((uint *)(gpio_p0 + gpio_outset)) = 1 << led_builtin;
 
     counter = 1000;
     while (counter) {
     }
 
-    *((unsigned int *)(gpio_p0 + gpio_outclr)) = 1 << led_builtin;
+    *((uint *)(gpio_p0 + gpio_outclr)) = 1 << led_builtin;
 
     counter = 1000;
     while (counter) {
