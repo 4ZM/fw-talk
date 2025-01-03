dde7cc94533a78e5dc50eddd283399287e9b952e
Anders Sundman (asum) <anders.sundman@tobii.com>
2025-01-03 13:06:29 +0100

    blink rust impl

--- nano-rust/README.md
+++ nano-rust/README.md
@@ -9,3 +9,8 @@ $ cargo generate --git https://github.com/rust-embedded/cortex-m-quickstart
 
 Run in gdb:
 $ gdb-multiarch -x openocd.gdb target/thumbv7m-none-eabi/debug/nano-rust
+
+or just "cargo run"
+
+Use probe-rs to flash (https://probe.rs/docs/tools/cargo-flash/):
+$ cargo flash --release --chip nRF52840_xxAA
--- nano-rust/src/main.rs
+++ nano-rust/src/main.rs
@@ -3,12 +3,56 @@
 
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 
-use cortex_m::asm;
-use cortex_m_rt::entry;
+use cortex_m::peripheral::syst::SystClkSource;
+use cortex_m::peripheral::Peripherals;
+use cortex_m_rt::{entry, exception};
+
+static mut COUNTER: u32 = 0;
+
+#[exception]
+fn SysTick() {
+    unsafe {
+        if COUNTER > 0 {
+            COUNTER -= 1;
+        }
+    }
+}
+
+fn delay(ms: u32) {
+    unsafe {
+        COUNTER = ms;
+        while COUNTER > 0 {}
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    const MS_TICK: u32 = 0xfa00; // 64MHz x 1ms => 64000 (0xfa00) ticks
+
+    let mut peripherals = Peripherals::take().unwrap();
+    let syst = &mut peripherals.SYST;
+    syst.set_clock_source(SystClkSource::Core);
+    syst.set_reload(MS_TICK);  // Set the SysTick interrupt to trigger every 1ms
+    syst.clear_current();
+    syst.enable_interrupt();
+    syst.enable_counter();
+
+    const GPIO_P0_DIRSET: *mut u32 = 0x5000_0518 as *mut u32;
+    const GPIO_P0_OUTSET: *mut u32 = 0x5000_0508 as *mut u32;
+    const GPIO_P0_OUTCLR: *mut u32 = 0x5000_050c as *mut u32;
+    const GPIO_PIN_LED_BUILTIN: u32 = 13;
+
+    unsafe {
+        core::ptr::write_volatile(GPIO_P0_DIRSET, 1 << GPIO_PIN_LED_BUILTIN);
+    }
+
     loop {
+        unsafe {
+            core::ptr::write_volatile(GPIO_P0_OUTSET, 1 << GPIO_PIN_LED_BUILTIN);
+            delay(1000);
+
+            core::ptr::write_volatile(GPIO_P0_OUTCLR, 1 << GPIO_PIN_LED_BUILTIN);
+            delay(1000);
+        }
     }
 }
